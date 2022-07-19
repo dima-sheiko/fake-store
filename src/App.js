0d7a6aa8d59762138a1e42ad02b6e1ff71e3dc3d
dima-sheiko <sheydim@gmail.com>
2022-07-19 14:38:21 +0300

    Establish routing system

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import About from './components/About';
+import Cart from './components/Cart';
 import './styles/style.css';
 import './styles/normalize.css';
 
@@ -12,9 +13,10 @@ function App() {
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='shop' element={<Shop/>} />
-        <Route path='about' element={<About/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='about' element={<About />} />
+        <Route path='shopping-cart' element={<Cart />} />
       </Routes>
     </>
   );
