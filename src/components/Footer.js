fadcbc42b661936f64cc5e71f8018ea3b2a0a839
dima-sheiko <sheydim@gmail.com>
2022-07-20 19:28:29 +0300

    Set up & style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import background from '../assets/background-wave.png';
 
 function Footer() {
-  return <div>Footer</div>;
+  const location = useLocation();
+
+  return (
+    <footer>
+      {location.pathname === '/shop' ? null : (
+        <FooterImage src={background} alt='footer image' />
+      )}
+    </footer>
+  );
 }
 
 export default Footer;
+
+/* Styles */
+
+const FooterImage = styled.img`
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  z-index: -1;
+`;
