77115d5b05ddb7fffbb689f52c54e42222ac69fb
dima-sheiko <sheydim@gmail.com>
2022-07-20 14:14:48 +0300

    Set up & style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import bag from '../assets/bag.svg';
 
 function Navbar() {
   return (
-    <div>Nav</div>
-  )
+    <Header>
+      <HeaderWrapper>
+        <Link to='/'>
+          <Title>FakeStore</Title>
+        </Link>
+        <Nav>
+          <NavbarLink to='/'>Home</NavbarLink>
+          <NavbarLink to='/shop'>Shop</NavbarLink>
+          <NavbarLink to='/about'>About</NavbarLink>
+          <NavbarLink to='/shopping-cart'>
+            <CartWrapper>
+              <CartCount>0</CartCount>
+              <CartIcon src={bag} alt='bag-icon' />
+            </CartWrapper>
+          </NavbarLink>
+        </Nav>
+      </HeaderWrapper>
+    </Header>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+/* Styles */
+
+const Header = styled.header`
+  background-color: #000000;
+  color: #fff;
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1350px;
+  margin: 0 auto;
+  padding: 40px;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 60px;
+  color: #46ffd3;
+  transition: 0.5s;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 60px;
+  font-size: 25px;
+`;
+
+const NavbarLink = styled(NavLink)`
+  color: #fff;
+  transition: 0.5s;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.5s;
+  }
+`;
+
+const CartWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  transition: 0.5s;
+
+  &:hover {
+    transform: scale(1.01);
+    transition: 0.5s;
+  }
+`;
+
+const CartCount = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 37px;
+  right: 37px;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  background-color: #46ffd3;
+  font-size: 18px;
+  font-weight: bold;
+  color: #000000;
+`;
+
+const CartIcon = styled.img`
+  width: 55px;
+`;
