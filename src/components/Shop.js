475f44a384db5776bee59c984050b0d3cc291d4d
dima-sheiko <sheydim@gmail.com>
2022-07-21 21:45:38 +0300

    Set up & style the shop section

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import data from '../data';
+import ShopItem from './ShopItem';
 
 function Shop() {
+  const { products } = data;
+
   return (
-    <div>Shop</div>
-  )
+    <Main>
+      <ItemsWrapper>
+        {products.map((product) => (
+          <ShopItem key={product.id} product={product}></ShopItem>
+        ))}
+      </ItemsWrapper>
+    </Main>
+  );
 }
 
-export default Shop
+export default Shop;
+
+/* Styles */
+
+const Main = styled.main`
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 40px;
+`;
+
+const ItemsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 40px;
+  margin-top: 20px;
+  animation: fadeIn 2s;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+`;
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-function ShopItem() {
+function ShopItem({ product }) {
   return (
-    <div>ShopItem</div>
-  )
+    <Item>
+      <ImageWrapper>
+        <ItemImage src={product.image} alt='product image' />
+      </ImageWrapper>
+      <DescriptionWrapper>
+        <ItemName>{product.title}</ItemName>
+        <ItemPrice>${product.price}</ItemPrice>
+        <AddBtn>Add To Cart</AddBtn>
+      </DescriptionWrapper>
+    </Item>
+  );
 }
 
-export default ShopItem
+export default ShopItem;
+
+/* Styles */
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+  border: 1px solid #dad7cd;
+  border-radius: 10px;
+`;
+
+const ImageWrapper = styled.div`
+  height: 190px;
+  padding: 10px;
+  margin: 0 auto;
+`;
+
+const ItemImage = styled.img`
+  height: 100%;
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+  margin-top: 25px;
+  border-top: 1px solid #dad7cd;
+  width: 100%;
+`;
+
+const ItemName = styled.p`
+  margin-top: 15px;
+  font-size: 23px;
+  font-weight: bold;
+`;
+
+const ItemPrice = styled.p`
+  font-size: 18px;
+`;
+
+const AddBtn = styled.button`
+  width: 100%;
+  padding: 15px;
+  background-color: #000000;
+  color: #fff;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
