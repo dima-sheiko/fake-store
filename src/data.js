de355030289873a5419b3dc2355bf9159c09cce5
dima-sheiko <sheydim@gmail.com>
2022-07-21 20:47:02 +0300

    Add data file

---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      title: 'Backpack',
+      price: 109.95,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    },
+    {
+      id: 2,
+      title: 'T-Shirt',
+      price: 22.3,
+      image:
+        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    },
+    {
+      id: 3,
+      title: 'Mens Cotton Jacket',
+      price: 55.99,
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    },
+    {
+      id: 4,
+      title: 'Mens Casual Slim Fit',
+      price: 15.99,
+      image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    },
+    {
+      id: 5,
+      title: 'Women Windbreaker',
+      price: 39.99,
+      image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+    },
+    {
+      id: 6,
+      title: 'Women Short Sleeve',
+      price: 7.95,
+      image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
+    },
+  ],
+};
+
+export default data;
