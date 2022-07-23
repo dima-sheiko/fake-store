537374b3b65a42238b3209053f9951c5433c4346
dima-sheiko <sheydim@gmail.com>
2022-07-23 14:38:56 +0300

    Update footer for cart component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 
   return (
     <footer>
-      {location.pathname === '/shop' || location.pathname === '/cart' ? '' : (
+      {location.pathname === '/shop' || location.pathname === '/cart' ? null : (
         <FooterImage src={background} alt='footer image' />
       )}
     </footer>
